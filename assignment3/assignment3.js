e1d77cf8e5a941b4bf60d2a2c7b2f2534abd21d9
N Sony <71254514+SONY078@users.noreply.github.com>
2022-09-01 18:58:57 +0530

    Add files via upload

---
+++ assignment3/assignment3.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose")
+
+const url = "mongodb://127.0.0.1:27017/Human_resource"
+
+const dbSchema = mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  salary: Number,
+  department: String,
+  lastCompany: String,
+  lastSalary: Number,
+  overallExp: Number,
+  contactInfo: Number,
+  yearGrad: Number,
+  gradStream: String
+})
+
+var rec1 = [{"firstName": "John",
+"lastName": "Doe",
+"salary": "25000",
+"department": "HR",
+"lastCompany": "X",
+"lastSalary": "10000",
+"overallExp": "2",
+"contactInfo": "1234567890",
+"yearGrad": "2016",
+"gradStream": "CSE"
+}]
+
+const mongoose = require("mongoose")
+
+const url = "mongodb://127.0.0.1:27017/Human_resource"
+
+const dbSchema = mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  salary: Number,
+  department: String,
+  lastCompany: String,
+  lastSalary: Number,
+  overallExp: Number,
+  contactInfo: Number,
+  yearGrad: Number,
+  gradStream: String
+})
+
