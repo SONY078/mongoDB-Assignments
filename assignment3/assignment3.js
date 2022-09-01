751c71e0d3e61c8e9f4b0b629ea9d1b491e6ea84
N Sony <71254514+SONY078@users.noreply.github.com>
2022-09-01 19:00:13 +0530

    Update assignment3.js

--- assignment3/assignment3.js
+++ assignment3/assignment3.js
@@ -27,20 +27,7 @@ var rec1 = [{"firstName": "John",
 "gradStream": "CSE"
 }]
 
-const mongoose = require("mongoose")
 
-const url = "mongodb://127.0.0.1:27017/Human_resource"
 
-const dbSchema = mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  salary: Number,
-  department: String,
-  lastCompany: String,
-  lastSalary: Number,
-  overallExp: Number,
-  contactInfo: Number,
-  yearGrad: Number,
-  gradStream: String
-})
+
 
